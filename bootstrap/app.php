72f12a925f42a1e32824487cf4395ac261f68b0f
Your Name <you@example.com>
2023-11-05 05:47:34 +0200

    Model move

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 /*
 |--------------------------------------------------------------------------
 | Create The Application
